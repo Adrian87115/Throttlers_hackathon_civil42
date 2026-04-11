301a8ca9c4e29aec59b7e088c9ab4c571a0a3459
MrMati <mati.niedbala5@gmail.com>
2026-04-11 12:57:16 +0200

    remove front, add gitignore

--- python/app/front/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-function ProtectedRoute({ user, checkRole }) {
-  if (!user) return <Navigate to="/auth" replace />;
-  if (checkRole && !checkRole(user)) return <Navigate to="/home" replace />;
-  return <Outlet />;
-}
-
-export default ProtectedRoute;
